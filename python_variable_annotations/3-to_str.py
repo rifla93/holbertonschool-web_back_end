2cdfc8ebe888e4b80d7e52b886e42e9eb1d78c2c
rifla93 <ralfsemi@yahoo.com>
2024-04-23 12:20:23 +0200

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotations
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    function which takes a float n as argument
+    and returns the floor of the float
+    '''
+    return str(n)
