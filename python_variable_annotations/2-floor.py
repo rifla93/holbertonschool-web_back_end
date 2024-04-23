f452d75d00bfd3f09046bd134b1ca1883a6295f8
rifla93 <ralfsemi@yahoo.com>
2024-04-23 12:17:59 +0200

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotations
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    function which takes a float n as argument
+    and returns the floor of the float
+    '''
+    return math.floor(n)
