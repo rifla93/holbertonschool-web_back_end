1dc05e092cb3e0146d545393b2e16e97dea3a421
rifla93 <ralfsemi@yahoo.com>
2024-04-23 12:32:52 +0200

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotations
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    function which takes a float n as argument
+    and returns the floor of the float
+    '''
+    return sum(input_list)
